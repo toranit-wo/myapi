361d237742ae9eab30652a51acfcad4d3c5c2c05
toranit-wo <toranit.wo.60@ubu.ac.th>
2021-08-26 08:38:24 +0700

    king

--- apis/serializers.py
+++ apis/serializers.py
@@ -9,13 +9,13 @@ class ApisSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         """
-        Create and return a new `Snippet` instance, given the validated data.
+        Create and return a new `Apis` instance, given the validated data.
         """
         return Apis.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
-        Update and return an existing `Snippet` instance, given the validated data.
+        Update and return an existing `Apis` instance, given the validated data.
         """
         instance.title = validated_data.get('title', instance.title)
         instance.data = validated_data.get('data', instance.data)
